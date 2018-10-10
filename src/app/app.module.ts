2c6d43d3e7710644c032a067ef72226d4ad3796f
vicovico233 <39315911+vicovico233@users.noreply.github.com>
2018-10-10 11:58:51 -0400

    until pag 65 on the book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    JokeListComponent
   ],
   imports: [
     BrowserModule
